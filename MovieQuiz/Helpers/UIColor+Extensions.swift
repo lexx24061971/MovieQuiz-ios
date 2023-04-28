5b7c03c634ce6ed21e4d0e8c780ffe9e36dce4c3
lexx24061971 <130874209+lexx24061971@users.noreply.github.com>
2023-04-29 00:11:39 +0400

    Исправлена ошибка

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -6,5 +6,5 @@ extension UIColor {
     static var ypRed: UIColor {UIColor(named:"YP Red") ?? UIColor.red}
     static var ypGreen: UIColor {UIColor(named:"YP Green") ?? UIColor.green}
     static var ypGray: UIColor {UIColor(named:"YP Gray") ?? UIColor.gray}
-    static var ypBackground: UIColor {UIColor(named:"YP Background") ?? UIColor.background}
+    static var ypBackground: UIColor {UIColor(named:"YP Background") ?? UIColor.darkGray}
 }
